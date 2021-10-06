243e3868a4864eb7b6ab01106910b6dffc400071
Djeniffer Vidal <74217321+Djenifer-Vidal@users.noreply.github.com>
2021-10-06 20:46:10 -0300

    Update Projeto Loja.sql

--- Projeto MysQl - Loja/Projeto Loja.sql
+++ Projeto MysQl - Loja/Projeto Loja.sql
@@ -1,7 +1,7 @@
 /** Atividade: Carrinho de Compras
 autor Djeniffer vidal 10/2021
 */
--- decimal usar decimal(10,2)
+
 show databases;
 create database dbloja;
 use dbloja;
@@ -15,28 +15,21 @@ quantidade int not null,
 valor decimal(10,2) not null
 );
 
--- drop table carrinho;
 
 describe carrinho;
 
--- alterar o nome de um campo na tabela
 alter table carrinho change carrinho produto varchar(30) not null;
 
--- adicionar um novo campo na tabela
 alter table carrinho add column OBS varchar(250);
 
--- adicionar um novo campo (coluna ) em um local especifico na tabela
 alter table carrinho add column OBS2 varchar(150) after OBS;
 
--- modificar o tipo de dados e/ou validação na coluna
 alter table carrinho modify column OBS2 varchar(150);
 
--- excluir uma coluna da tabela
 alter table carrinho drop column OBS2;
 alter table carrinho drop column OBS;
 
 
--- CRUD CREATE - criar dados
 insert into carrinho (produto, quantidade, valor) values ('macarrao',100, 15.00);
 insert into carrinho (produto, quantidade, valor) values ('Feijão',50, 20.50);
 insert into carrinho (produto, quantidade, valor) values ('Arroz',46, 15.30);
@@ -46,45 +39,29 @@ insert into carrinho (produto, quantidade, valor) values ('Tomate',130, 2.00);
 insert into carrinho (produto, quantidade, valor) values ('Pimentão',150, 5.00);
 insert into carrinho (produto, quantidade, valor) values ('Cola_Cola LT2',200, 12.00);
 
--- Seleionar todos os regitros (dados) da tabela
 select *from carrinho;
  
- -- Operações matematicas no banco de dados - Calcular o resultado
  select sum(quantidade * valor) as Total from carrinho;
  
- -- Selecionar colunas especificas da tabela
 select produto, valor from carrinho;
 select produto from carrinho;
  
- -- Selecionar colunas em ordem crescente e decrescente
- -- Ordem decrescente
 select codigo, produto from carrinho order by codigo desc;
--- Ordem crescente
 select codigo, produto from carrinho order by codigo asc;
 
--- Uso de filtros
 select *from carrinho where codigo=3;
 select *from carrinho where produto='macarrão';
 
--- Realizar pesquisa por letras em comum
 select *from carrinho where produto like 'ma%';
 
--- **CRUD READ - pesquisar dados
--- Selecionar todos os registros (dados) da tabela
 select *from carrinho;
 
--- **CRUD UPDATE - editar contato 
 update carrinho set quantidade= 500 where codigo= 1;
 update carrinho set produto= 'Macarrão', quantidade= 200, valor= 25.30 where codigo= 1;
 update carrinho set produto='Feijão', valor=15.50 where codigo= 2;
 
--- **CRUD Delete - exluir dados
--- Atenção!! Não esquecer de usar o WHERE --- IMPORTANTE
 delete from carrinho where codigo= 3;
 
--- Criar Nova tabela Estoque
--- timestamp default current_timestamp - (data e hora automatica)
--- date (tipo de dados data) YYYYMMDD
 create table estoque(
 codigo int primary key auto_increment,
 barcode varchar(50) unique,
@@ -125,36 +102,25 @@ update estoque set quantidade= 200 where codigo=3;
 update estoque set quantidade= 100 where codigo=1;
 
 
--- inventario do estoque (total)
 select sum(quantidade * valor) as Total from estoque;
  
- -- Relatorio de reposição do estoque EX:1
  select *from estoque where quantidade < estoquemin;
  
-  -- Relatorio de reposição do estoque EX:2
-  -- date_format --> formatar a exibição da data
-  -- %d- dia/ %m - Mes/ %Y- Ano
 select codigo as código, produto, date_format(dataval, '%d/%m/%Y') as data_validade, quantidade, estoquemin as estoque_minimo from estoque 
 where quantidade < estoquemin;
  
- -- Relatório de produtos vencidos 1 (validade dos produtos)
 select codigo as código,produto,date_format(dataval,'%d/%m/%Y') as data_validade
 from estoque;
 
--- Relatório de validade produtos 2
--- datediff() retorna a diferença em dias de duas datas
--- curdate() data autal
 select codigo as código,produto,date_format(dataval, '%d/%m/%Y') as data_validade,
 datediff(dataval,curdate()) as dias_restantes from estoque;
 
--- Fazer UPDATE
 update estoque set estoquemin =15 where codigo = 3;
 
 update estoque set produto = 'Farinha', fabricante ='nestle*',dataval=20220330,quantidade=250, estoquemin=20, valor=5.99 where codigo = 10;
  
  select * from estoque;
  
- -- DELETE UM INTEM
 delete from estoque where codigo = 6;
 
 select * from estoque;
